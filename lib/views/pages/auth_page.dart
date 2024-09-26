9138de4de32009bfaaa3930554351728467bff52
Mostafa Noaman <nondy1425@gmail.com>
2024-09-26 17:48:59 +0300

    wrapped the page with a consumer to handle logic and states

--- lib/views/pages/auth_page.dart
+++ lib/views/pages/auth_page.dart
@@ -1,4 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shooppyy/controllers/auth_controller.dart';
+import 'package:shooppyy/services/auth.dart';
 import 'package:shooppyy/utilities/enums.dart';
 import 'package:shooppyy/utilities/routes.dart';
 import 'package:shooppyy/views/widgets/main_button.dart';
@@ -14,7 +18,6 @@ class _AuthPageState extends State<AuthPage> {
   final GlobalKey<FormState> _formKey = GlobalKey();
   final TextEditingController _email = TextEditingController();
   final TextEditingController _password = TextEditingController();
-  var _authType = AuthFormType.login;
   final _emailFocusNode = FocusNode();
   final _passwordFocusNode = FocusNode();
 
@@ -26,137 +29,163 @@ class _AuthPageState extends State<AuthPage> {
     _password.dispose();
   }
 
+  Future<void> _submit(AuthController model) async {
+    try {
+      await model.submit();
+      if (!mounted) return;
+      Navigator.of(context).pushNamed(AppRoutes.bottomNavBarRoute);
+    } on FirebaseAuthException catch (e) {
+      showDialog(
+          context: context,
+          builder: (_) => AlertDialog(
+                title: const Text('Error!'),
+                content: Text(e.toString()),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text('close'),
+                  ),
+                ],
+              ));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 24.0, horizontal: 32.0),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    _authType == AuthFormType.login ? 'Login' : 'Register',
-                    style: Theme.of(context).textTheme.headlineLarge,
-                  ),
-                  const SizedBox(height: 64),
-                  TextFormField(
-                    controller: _email,
-                    focusNode: _emailFocusNode,
-                    onEditingComplete: () =>
-                        FocusScope.of(context).requestFocus(_passwordFocusNode),
-                    textInputAction: TextInputAction.next,
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'email can\'t be empty';
-                      }
-                      return null;
-                    },
-                    decoration: const InputDecoration(
-                      labelText: 'Email',
-                      hintText: 'Enter your email',
-                    ),
-                  ),
-                  const SizedBox(height: 24),
-                  TextFormField(
-                    controller: _password,
-                    focusNode: _passwordFocusNode,
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'password can\'t be empty';
-                      }
-                      return null;
-                    },
-                    decoration: const InputDecoration(
-                      labelText: 'Password',
-                      hintText: 'Enter your password',
-                    ),
-                  ),
-                  SizedBox(height: 24),
-                  if (_authType == AuthFormType.login)
-                    Align(
-                      alignment: Alignment.topRight,
-                      child: InkWell(
-                        child: Text('Forgot your password?'),
-                        onTap: () {},
-                      ),
-                    ),
-                  SizedBox(height: 24),
-                  MainButton(
-                      text: _authType == AuthFormType.login
-                          ? 'Login'
-                          : 'Register',
-                      onTap: () {
-                        if (_formKey.currentState!.validate()) {
-                          Navigator.of(context)
-                              .pushNamed(AppRoutes.bottomNavBarRoute);
-                        }
-                      }),
-                  SizedBox(height: 16),
-                  Align(
-                    alignment: Alignment.center,
-                    child: InkWell(
-                      child: Text(_authType == AuthFormType.login
-                          ? 'Don\'t have an account? Register'
-                          : 'Already have an account ? Login'),
-                      onTap: () {
-                        _formKey.currentState!.reset();
-                        setState(() {
-                          if (_authType == AuthFormType.login) {
-                            _authType = AuthFormType.register;
-                          } else {
-                            _authType = AuthFormType.login;
-                          }
-                        });
-                      },
-                    ),
-                  ),
-                  SizedBox(height: size.height * 0.1),
-                  Align(
-                    alignment: Alignment.center,
-                    child: Text(
-                      _authType == AuthFormType.login
-                          ? 'Or login with'
-                          : 'Or Register with',
-                      style: Theme.of(context).textTheme.bodyMedium,
-                    ),
-                  ),
-                  SizedBox(height: 16),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+    final auth = Provider.of<AuthBase>(context);
+    return Consumer<AuthController>(
+      builder: (_, model, __) {
+        return Scaffold(
+          body: SafeArea(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 24.0, horizontal: 32.0),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Container(
-                        height: 80,
-                        width: 80,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(16),
-                          color: Colors.white,
-                        ),
-                        child: Icon(Icons.facebook),
+                      Text(
+                        model.authFormType == AuthFormType.login
+                            ? 'Login'
+                            : 'Register',
+                        style: Theme.of(context).textTheme.headlineLarge,
                       ),
-                      SizedBox(width: 10),
-                      Container(
-                        height: 80,
-                        width: 80,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(16),
-                          color: Colors.white,
+                      const SizedBox(height: 64),
+                      TextFormField(
+                        controller: _email,
+                        focusNode: _emailFocusNode,
+                        onEditingComplete: () => FocusScope.of(context)
+                            .requestFocus(_passwordFocusNode),
+                        onChanged: model.updateEmail,
+                        textInputAction: TextInputAction.next,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'email can\'t be empty';
+                          }
+                          return null;
+                        },
+                        decoration: const InputDecoration(
+                          labelText: 'Email',
+                          hintText: 'Enter your email',
                         ),
-                        child: Icon(Icons.telegram),
+                      ),
+                      const SizedBox(height: 24),
+                      TextFormField(
+                        controller: _password,
+                        focusNode: _passwordFocusNode,
+                        onChanged: model.updatePassword,
+                        obscureText: true,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'password can\'t be empty';
+                          }
+                          return null;
+                        },
+                        decoration: const InputDecoration(
+                          labelText: 'Password',
+                          hintText: 'Enter your password',
+                        ),
+                      ),
+                      const SizedBox(height: 24),
+                      if (model.authFormType == AuthFormType.login)
+                        Align(
+                          alignment: Alignment.topRight,
+                          child: InkWell(
+                            child: const Text('Forgot your password?'),
+                            onTap: () {},
+                          ),
+                        ),
+                      const SizedBox(height: 24),
+                      MainButton(
+                          text: model.authFormType == AuthFormType.login
+                              ? 'Login'
+                              : 'Register',
+                          onTap: () {
+                            if (_formKey.currentState!.validate()) {
+                              _submit(model);
+                            }
+                          }),
+                      const SizedBox(height: 16),
+                      Align(
+                        alignment: Alignment.center,
+                        child: InkWell(
+                          child: Text(model.authFormType == AuthFormType.login
+                              ? 'Don\'t have an account? Register'
+                              : 'Already have an account ? Login'),
+                          onTap: () {
+                            _formKey.currentState!.reset();
+                            model.toggleFormType();
+                          },
+                        ),
+                      ),
+                      SizedBox(height: size.height * 0.1),
+                      Align(
+                        alignment: Alignment.center,
+                        child: Text(
+                          model.authFormType == AuthFormType.login
+                              ? 'Or login with'
+                              : 'Or Register with',
+                          style: Theme.of(context).textTheme.bodyMedium,
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            height: 80,
+                            width: 80,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(16),
+                              color: Colors.white,
+                            ),
+                            child: const Icon(Icons.facebook),
+                          ),
+                          const SizedBox(width: 10),
+                          Container(
+                            height: 80,
+                            width: 80,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(16),
+                              color: Colors.white,
+                            ),
+                            child: const Icon(Icons.telegram),
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                ],
+                ),
               ),
             ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
