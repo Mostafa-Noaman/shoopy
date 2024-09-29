7dbb6b9def24c0b02565aa652b28afaddab463c8
Mostafa Noaman <nondy1425@gmail.com>
2024-09-30 02:04:06 +0300

    made some changes to the products stream methods

--- lib/controllers/database_controller.dart
+++ lib/controllers/database_controller.dart
@@ -1,16 +1,28 @@
 import 'package:shooppyy/models/product_model.dart';
 import 'package:shooppyy/services/firestore_services.dart';
+import 'package:shooppyy/utilities/api_path.dart';
 
 abstract class Database {
-  Stream<List<ProductModel>> productStream();
+  Stream<List<ProductModel>> salesProductStream();
+
+  Stream<List<ProductModel>> newProductStream();
 }
 
 class FireStoreDatabase implements Database {
   final _service = FireStoreServices.instance;
+  final String uId;
+
+  FireStoreDatabase(this.uId);
 
   @override
-  Stream<List<ProductModel>> productStream() => _service.collectionStream(
-        path: 'products/',
+  Stream<List<ProductModel>> salesProductStream() => _service.collectionStream(
+      path: ApiPath.products,
+      builder: (data, documentId) => ProductModel.fromMap(data!, documentId),
+      queryBuilder: (query) => query.where('discountValue', isNotEqualTo: 0));
+
+  @override
+  Stream<List<ProductModel>> newProductStream() => _service.collectionStream(
+        path: ApiPath.products,
         builder: (data, documentId) => ProductModel.fromMap(data!, documentId),
       );
 }
