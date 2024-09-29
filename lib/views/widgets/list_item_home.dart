b5a915ce7a9a91d643afd79764e21a0da6f918ae
Mostafa Noaman <nondy1425@gmail.com>
2024-09-30 02:01:33 +0300

    fixed product image not showing

--- lib/views/widgets/list_item_home.dart
+++ lib/views/widgets/list_item_home.dart
@@ -21,7 +21,7 @@ class ListItemHome extends StatelessWidget {
                 ClipRRect(
                   borderRadius: BorderRadius.circular(10),
                   child: Image.network(
-                    AppAssets.tempProductImage,
+                    product.imageUrl,
                     height: 200,
                     fit: BoxFit.cover,
                   ),
