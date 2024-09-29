a74743a3649b7d90c4b238f05d8705ebcb0e4690
Mostafa Noaman <nondy1425@gmail.com>
2024-09-30 02:03:25 +0300

    fixed the navigation to the home page

--- lib/views/pages/landing_page.dart
+++ lib/views/pages/landing_page.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shooppyy/controllers/auth_controller.dart';
+import 'package:shooppyy/controllers/database_controller.dart';
 import 'package:shooppyy/services/auth.dart';
 import 'package:shooppyy/views/pages/auth_page.dart';
 import 'package:shooppyy/views/pages/bottom_nav_bar.dart';
@@ -24,7 +25,12 @@ class LandingPage extends StatelessWidget {
               child: const AuthPage(),
             );
           }
-          return const BottomNavBar();
+          return ChangeNotifierProvider<AuthController>(
+            create: (_) => AuthController(auth: auth),
+            child: Provider<Database>(
+                create: (_) => FireStoreDatabase(user.uid),
+                child: const BottomNavBar()),
+          );
         }
         return const Scaffold(
           body: Center(
