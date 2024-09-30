e3eacd2e664a9e13b51852473ebd0e0cc2cd4341
Mostafa Noaman <nondy1425@gmail.com>
2024-09-30 03:12:39 +0300

    made the item clickable

--- lib/views/widgets/list_item_home.dart
+++ lib/views/widgets/list_item_home.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shooppyy/models/product_model.dart';
-import 'package:shooppyy/utilities/app_assets.dart';
+import 'package:shooppyy/utilities/routes.dart';
 
 class ListItemHome extends StatelessWidget {
   const ListItemHome({super.key, required this.product});
@@ -9,63 +9,69 @@ class ListItemHome extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DecoratedBox(
-      decoration: BoxDecoration(),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 3),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Stack(
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: Image.network(
-                    product.imageUrl,
-                    height: 200,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                  width: 35,
-                  child: DecoratedBox(
-                    decoration: BoxDecoration(
-                        color: Colors.red,
-                        borderRadius: BorderRadius.circular(15)),
-                    child: Center(
-                      child: Text(
-                        '${product.discount.toString()}%',
-                        style: Theme.of(context)
-                            .textTheme
-                            .bodySmall
-                            ?.copyWith(color: Colors.white),
-                      ),
+    return InkWell(
+      onTap: () {
+        Navigator.of(context, rootNavigator: true)
+            .pushNamed(AppRoutes.productDetailsRoute, arguments: product);
+      },
+      child: DecoratedBox(
+        decoration: const BoxDecoration(),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 3),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Stack(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.network(
+                      product.imageUrl,
+                      height: 200,
+                      fit: BoxFit.cover,
                     ),
                   ),
-                )
-              ],
-            ),
-            const SizedBox(height: 8),
-            Text(
-              product.category,
-              style: Theme.of(context)
-                  .textTheme
-                  .labelMedium
-                  ?.copyWith(color: Colors.grey),
-            ),
-            Text(
-              product.title,
-              style: Theme.of(context).textTheme.labelLarge,
-            ),
-            Text(
-              '${product.price.toString()}\$',
-              style: Theme.of(context)
-                  .textTheme
-                  .labelSmall
-                  ?.copyWith(color: Colors.grey),
-            ),
-          ],
+                  SizedBox(
+                    height: 20,
+                    width: 35,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.circular(15)),
+                      child: Center(
+                        child: Text(
+                          '${product.discount.toString()}%',
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodySmall
+                              ?.copyWith(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(height: 8),
+              Text(
+                product.category,
+                style: Theme.of(context)
+                    .textTheme
+                    .labelMedium
+                    ?.copyWith(color: Colors.grey),
+              ),
+              Text(
+                product.title,
+                style: Theme.of(context).textTheme.labelLarge,
+              ),
+              Text(
+                '${product.price.toString()}\$',
+                style: Theme.of(context)
+                    .textTheme
+                    .labelSmall
+                    ?.copyWith(color: Colors.grey),
+              ),
+            ],
+          ),
         ),
       ),
     );
