9886218af36bc31d7718a43bceb2499eab16d067
Mostafa Noaman <nondy1425@gmail.com>
2024-10-01 02:51:45 +0300

    added functions to -> save user data && add product to cart &&  products cart collection

--- lib/controllers/database_controller.dart
+++ lib/controllers/database_controller.dart
@@ -1,4 +1,6 @@
 import 'package:shooppyy/models/product_model.dart';
+import 'package:shooppyy/models/cart_model.dart';
+import 'package:shooppyy/models/user_model.dart';
 import 'package:shooppyy/services/firestore_services.dart';
 import 'package:shooppyy/utilities/api_path.dart';
 
@@ -6,6 +8,12 @@ abstract class Database {
   Stream<List<ProductModel>> salesProductStream();
 
   Stream<List<ProductModel>> newProductStream();
+
+  Stream<List<CartModel>> myProductsCart();
+
+  Future<void> setUserData(UserModel userData);
+
+  Future<void> addToCart(CartModel product);
 }
 
 class FireStoreDatabase implements Database {
@@ -24,5 +32,22 @@ class FireStoreDatabase implements Database {
   Stream<List<ProductModel>> newProductStream() => _service.collectionStream(
         path: ApiPath.products,
         builder: (data, documentId) => ProductModel.fromMap(data!, documentId),
+      );
+
+  @override
+  Future<void> setUserData(UserModel userData) async => await _service.setData(
+      path: ApiPath.user(userData.uId), data: userData.toMap());
+
+  @override
+  Future<void> addToCart(CartModel product) async => await _service.setData(
+      path: ApiPath.addToCart(uId, product.id), data: product.toMap());
+
+  @override
+  Stream<List<CartModel>> myProductsCart() => _service.collectionStream(
+        path: ApiPath.myProductCart(uId),
+        builder: (data, documentId) => CartModel.fromMap(
+          data!,
+          documentId,
+        ),
       );
 }
