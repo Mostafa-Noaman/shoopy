e7156edc2e2098736cb6356461c648751bdd628f
Mostafa Noaman <nondy1425@gmail.com>
2024-10-07 03:43:18 +0300

    added address path

--- lib/utilities/api_path.dart
+++ lib/utilities/api_path.dart
@@ -1,9 +1,16 @@
 class ApiPath {
   static String products = 'products/';
+
   static String user(String uid) => 'users/$uid';
+
   static String addToCart(String uid, String addToCartId) =>
       'users/$uid/cart/$addToCartId';
+
   static String myProductCart(String uid) => 'users/$uid/cart/';
   static String deliveryMethods = 'deliveryMethods/';
+
   static String shippingAddress(String uid) => 'users/$uid/shippingAddresses';
+
+  static String saveAddress(String uid, String addressId) =>
+      'users/$uid/shippingAddresses/$addressId';
 }
